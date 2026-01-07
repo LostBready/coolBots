327fe0fea0c07d6c17a0fc0442fab9ccc08637f1
Hlebushek <darkman.chel228@gmail.com>
2026-01-07 07:57:33 +0300

    updated bad_bot to v0.2

--- bad_bot.js
+++ bad_bot.js
@@ -1,1 +1,3 @@
-console.log("I am a bad bot and I gonna destroy this pathetic world, LOL")
+console.log("I am a bad bot and I gonna destroy this pathetic world, LOL");
+await(1000);
+console.log("Hahahah, you are so weak, xD");
